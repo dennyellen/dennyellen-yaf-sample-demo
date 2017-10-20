587f4aff1dd1d0a6049394a9b6914bd165ef66b6
dennyellen <tuqian@hotmail.com>
2017-10-21 00:48:18 +0800

    fix Yaf check

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use Yaf\Application;
 // 验证yaf扩展已安装
 if(!extension_loaded('yaf')){
-    return 'Yaf is not loaded !';
+     exit('Yaf is not loaded !');
 }else{
     define('APPLICATION_PATH', dirname(__DIR__));
     $application = new Application( APPLICATION_PATH . "/conf/app.ini");
