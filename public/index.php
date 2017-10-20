4797acf29005c0cb0ff25b8e9c76cf8d258b551d
dennyellen <tuqian@hotmail.com>
2017-10-20 16:00:27 +0800

    add extension check

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 
 use Yaf\Application;
-define('APPLICATION_PATH', dirname(__DIR__));
-$application = new Application( APPLICATION_PATH . "/conf/app.ini");
-$application->bootstrap()->run();
+// 验证yaf扩展已安装
+if(!extension_loaded('yaf')){
+    return 'Yaf is not loaded !';
+}else{
+    define('APPLICATION_PATH', dirname(__DIR__));
+    $application = new Application( APPLICATION_PATH . "/conf/app.ini");
+    $application->bootstrap()->run();
+}
